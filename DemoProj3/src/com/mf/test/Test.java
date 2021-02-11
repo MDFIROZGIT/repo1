4462f9b7e7fadcc4ba4f1a3c3825b6a6fc6d4dc9
firoz <Dev2@gmail.com>
2021-02-11 10:53:13 +0530

    dev1 modified Test.java

--- DemoProj3/src/com/mf/test/Test.java
+++ DemoProj3/src/com/mf/test/Test.java
@@ -1,5 +1,11 @@
 package com.mf.test;
 
+import com.mf.utility.Utility;
+
 public class Test {
 
+	public static void main(String[] args) {
+		Utility ut=new Utility();
+		System.out.println(ut.date());
+	}
 }
